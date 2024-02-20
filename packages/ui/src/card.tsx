0e9ddb2af31f682d0f597fd2a3dc86d9c4e48d52
0xdbe <23268141+0xdbe@users.noreply.github.com>
2024-02-20 15:56:31 +0100

    fix(ui): update gretting message v12

--- packages/ui/src/card.tsx
+++ packages/ui/src/card.tsx
@@ -19,7 +19,7 @@ export function Card({
       <h2>
         {title} <span>-&gt;</span>
       </h2>
-      <p>Hello World 11</p>
+      <p>Hello World 12</p>
       <p>{children}</p>
     </a>
   );

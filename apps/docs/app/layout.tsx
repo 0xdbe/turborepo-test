75da82ec26fbe46c4ad8750769e65a72d4c64a3b
0xdbe <23268141+0xdbe@users.noreply.github.com>
2024-04-03 17:57:18 +0200

    fix: update ci release

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }): JSX.Element {
   return (
     <html lang="en">
-      <p>hello qrrssssrrrrr</p>
+      <p>hello qrrsssssrrrrr</p>
       <body className={inter.className}>{children}</body>
     </html>
   );
